b65eaee4fc05096123dd9223b6f9c2a869bbb6ac
Anastasia <n41111215@mail.ru>
2020-05-08 14:43:37 +0300

    Добавила printf

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public static void main(String [] args) {
     double weight = 63;
     double height = 1.73;
     double bmi=service.calculate(weight,height);
-    System.out.println(bmi);
+    System.out.printf("Индекс массы тела %.1f",bmi);
 }
 }
